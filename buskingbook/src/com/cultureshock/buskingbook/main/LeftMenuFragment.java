3d561fa5306f6dca30648b914bc41a0224fe261c
yup <yup@192.168.0.12>
2013-09-30 01:50:29 +0900

    1. update left menu layout xml 2. add strings for menu.

--- buskingbook/src/com/cultureshock/buskingbook/main/LeftMenuFragment.java
+++ buskingbook/src/com/cultureshock/buskingbook/main/LeftMenuFragment.java
@@ -1,4 +1,3 @@
-
 package com.cultureshock.buskingbook.main;
 
 import android.app.Activity;
@@ -11,26 +10,25 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.cultureshock.buskingbook.R;
-import com.cultureshock.buskingbook.page.MainHomeFragment;
-
 
 public class LeftMenuFragment extends Fragment implements OnClickListener {
     private static Activity mActivity;
     private static LeftMenuFragment mInstance;
-    private TextView m_oBtnTest;
-    private TextView m_oBtnHome;
-
+    private TextView mBtnTempMusician;
+    private TextView mBtnTempNormal;
 
     public static LeftMenuFragment getInstance() {
-        if ( mInstance == null ) {
+        if (mInstance == null) {
             mInstance = new LeftMenuFragment();
         }
         return mInstance;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_left_menu, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_left_menu_musician, container,
+                false);
     }
 
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -38,55 +36,43 @@ public class LeftMenuFragment extends Fragment implements OnClickListener {
 
         mActivity = getActivity();
         mInstance = this;
-        m_oBtnTest = (TextView)getActivity().findViewById(R.id.test_left);
-        m_oBtnHome = (TextView)getActivity().findViewById(R.id.test_home);
-        m_oBtnTest.setOnClickListener(this);
-        m_oBtnHome.setOnClickListener(this);
+        mBtnTempMusician = (TextView) getActivity().findViewById(
+                R.id.temp_musician);
+        mBtnTempNormal = (TextView) getActivity()
+                .findViewById(R.id.temp_normal);
+        if (mBtnTempMusician != null) {
+            mBtnTempMusician.setOnClickListener(this);
+        }
+        if (mBtnTempNormal != null) {
+            mBtnTempNormal.setOnClickListener(this);
+        }
     }
 
     // the meat of switching the above fragment
     private void switchFragment(Fragment fragment) {
-        if ( getActivity() == null )
+        if (getActivity() == null)
             return;
 
-        if ( getActivity() instanceof BuskingMainActivity ) {
+        if (getActivity() instanceof BuskingMainActivity) {
             BuskingMainActivity fca = (BuskingMainActivity) getActivity();
             fca.switchContent(fragment);
         }
     }
 
-   
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see android.support.v4.app.Fragment#onResume()
      */
     @Override
     public void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
-        //loginSatatus();
+        // loginSatatus();
     }
-
-   
-
-   
 
     @Override
     public void onClick(View v) {
-        Fragment newContent = null;
-        switch ( v.getId() ) {
-	        case R.id.test_home :
-	        {
-	        	MainActivity.getInstance().replaceFragment(MainHomeFragment.class, null, false);
-	        	break;
-	        }
-	        case R.id.test_left :
-	        {
-//	        	MainActivity.getInstance().replaceFragment(MainHomeFragment.class, null, true);
-	        	break;
-	        }
-        }
     }
 
-   
 }
