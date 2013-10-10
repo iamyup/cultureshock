c5c29b91ebb09ae2243f9350d02fc051d6d0763d
yup <yup@192.168.1.2>
2013-10-11 01:39:34 +0900

    add facebook sdk.

--- buskingbook/src/com/cultureshock/buskingbook/main/LoginJoinActivity.java
+++ buskingbook/src/com/cultureshock/buskingbook/main/LoginJoinActivity.java
@@ -1,21 +1,16 @@
 package com.cultureshock.buskingbook.main;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.facebook.*;
+import com.facebook.model.*;
 import com.cultureshock.buskingbook.R;
-import com.cultureshock.buskingbook.framework.BaseActivity;
-import com.cultureshock.buskingbook.page.MainHomeFragment;
 
 public class LoginJoinActivity extends Activity implements View.OnClickListener {
     private Context mContext;
@@ -23,9 +18,10 @@ public class LoginJoinActivity extends Activity implements View.OnClickListener 
     private LinearLayout m_oBtnJoinFacefook;
     private LinearLayout m_oBtnJoinEmail;
     private LinearLayout m_oBtnLogin;
-    
+
     @Override
-    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+    public View onCreateView(View parent, String name, Context context,
+            AttributeSet attrs) {
         return super.onCreateView(parent, name, context, attrs);
     }
 
@@ -37,22 +33,22 @@ public class LoginJoinActivity extends Activity implements View.OnClickListener 
         mContext = this;
         mInstance = this;
         setUi();
-       
+
     }
-    public void setUi()
-    {
-    	m_oBtnJoinEmail = (LinearLayout)findViewById(R.id.btn_join_email);
-    	m_oBtnJoinFacefook = (LinearLayout)findViewById(R.id.btn_join_facebook);
-    	m_oBtnLogin = (LinearLayout)findViewById(R.id.btn_login);
-    	m_oBtnJoinEmail.setOnClickListener(this);
-    	m_oBtnJoinFacefook.setOnClickListener(this);
-    	m_oBtnLogin.setOnClickListener(this);
+
+    public void setUi() {
+        m_oBtnJoinEmail = (LinearLayout) findViewById(R.id.btn_join_email);
+        m_oBtnJoinFacefook = (LinearLayout) findViewById(R.id.btn_join_facebook);
+        m_oBtnLogin = (LinearLayout) findViewById(R.id.btn_login);
+        m_oBtnJoinEmail.setOnClickListener(this);
+        m_oBtnJoinFacefook.setOnClickListener(this);
+        m_oBtnLogin.setOnClickListener(this);
     }
+
     public static LoginJoinActivity getInstance() {
         return mInstance;
     }
 
-  
     @Override
     protected void onResume() {
         super.onResume();
@@ -67,33 +63,46 @@ public class LoginJoinActivity extends Activity implements View.OnClickListener 
 
     @Override
     protected void onDestroy() {
-       
+
         super.onDestroy();
     }
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		switch(v.getId())
-		{
-		case R.id.btn_join_facebook : 
-		{
-			break;
-		}
-		case R.id.btn_join_email : 
-		{
-			Intent intent =  new Intent(this.getApplicationContext(), JoinActivity.class);
-			startActivity(intent);
-			break;
-		}
-		case R.id.btn_login : 
-		{
-			Intent intent =  new Intent(this.getApplicationContext(), LoginActivity.class);
-			startActivity(intent);
-			break;
-		}
-		}
-		
-	}
-   
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+        case R.id.btn_join_facebook: {
+            // start Facebook Login
+            Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+              // callback when session changes state
+              @Override
+              public void call(Session session, SessionState state, Exception exception) {
+
+              }
+            });
+            break;
+        }
+        case R.id.btn_join_email: {
+            Intent intent = new Intent(this.getApplicationContext(),
+                    JoinActivity.class);
+            startActivity(intent);
+            break;
+        }
+        case R.id.btn_login: {
+            Intent intent = new Intent(this.getApplicationContext(),
+                    LoginActivity.class);
+            startActivity(intent);
+            break;
+        }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
 }
